d31ba8a8d005a32a49b559853aa3953d7f066a0b
Ritesh-div <riteshkumarsahu1345@gmail.com>
2025-06-08 19:04:13 +0530

    use cors

--- src/app.js
+++ src/app.js
@@ -6,8 +6,10 @@ const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
-
+app.use(cors({
+  origin: "https://your-frontend.netlify.app",
+  credentials: true,
+}));
 // Connect to MongoDB
 mongoose
   .connect("mongodb://localhost:27017/SparkX", {
